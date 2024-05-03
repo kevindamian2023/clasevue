4b5b417353263bbf1cbf42bd91c50dc3303f2f75
kevindamian2023 <martinezayalakevindamian@gmail.com>
2024-05-03 17:05:24 -0500

    classvue

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import  { ref } from "vue"
+
+let counter = ref(5)
+
+function add() {
+  counter.value++
+  console.log(counter)
+}
+</script>
+
+<template>
+  <div @click="add" class="text-white select-none hover:cursor-pointer bg-blue-500 rounded py-1 px-2 max-w-40 text-center">
+    Numero de clics: {{ counter }}
+  </div>
+</template>
